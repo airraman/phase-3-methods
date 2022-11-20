9fa63c504f7a7e48922cf54115a0219581e77f16
Raman <ramanmcreates@gmail.com>
2022-11-20 14:23:59 -0500

    commit tree b14b5969329c20996cd53f5aac8e31d050d86598

--- methods.rb
+++ methods.rb
@@ -1,1 +1,46 @@
 # Your code here!
+# function greetProgrammer() {
+#   console.log("Hello, programmer!");
+# }
+
+def greetProgrammer
+    puts "Hello, programmer"
+end
+
+# function greet(name) {
+#   console.log(`Hello, ${name}!`);
+# }
+
+def greet (name)
+    puts "Hello, #{name}"
+end
+
+# function greetWithDefault(name = "programmer") {
+#   console.log(`Hello, ${name}!`);
+# }
+
+def greetWithDefault(name = "programmer")
+    puts "Hello, #{name}"
+end
+
+# function add(num1, num2) {
+#   return num1 + num2;
+# }
+
+def add (num1, num2)
+    puts num1 + num2
+end
+
+
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+# }
+
+def halve (number)
+    if number.class != Integer || Float
+        return nil
+        number / 2
+    end
+
